18bf2bd816b9a9dd8e1c8e21365958e6936d0ca5
Jorge Novela <17460535@colima.tecnm.mx>
2021-01-26 11:21:47 -0600

    jo

--- app.py
+++ app.py
@@ -14,44 +14,56 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://bzuvvwhdddncot:0413c40db78fa
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:12345@localhost:5432/armando'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-class Alumno(db.Model): 
-    id = db.Column(db.Integer,primary_key=True)
+
+
+class Alumno(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(30))
-    apellido = db.Column(db.String(30)) 
+    apellido = db.Column(db.String(30))
     lista = []
-@app.route('/', methods=['GET','POST'])
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == "POST":
         campo_nombre = request.form['nombre']
         campo_apellido = request.form['apellido']
-        alumno = Alumno(nombre=campo_nombre,apellido=campo_apellido)
+        alumno = Alumno(nombre=campo_nombre, apellido=campo_apellido)
         db.session.add(alumno)
         db.session.commit()
         mensaje = "Registrado"
         return redirect(url_for('index'))
-    return render_template("index.html", variable = ["acerca", "Contacto", "Preguntas Frecuentes"])
+    return render_template("index.html", variable=["acerca", "Contacto", "Preguntas Frecuentes"])
+
+
 @app.route('/acerca')
 def acerca():
     consulta = Alumno.query.all()
-    return render_template("acerca.html", arg = consulta)
+    return render_template("acerca.html", arg=consulta)
+
+
 @app.route('/eliminar/<id>')
 def eliminar(id):
     q = Alumno.query.filter_by(id=int(id)).delete()
     db.session.commit()
     return redirect(url_for('acerca'))
 
+
 @app.route('/editar/<id>')
 def editar(id):
     r = Alumno.query.filter_by(id=int(id)).first()
-    return render_template("editar.html", alumno = r)
+    return render_template("editar.html", alumno=r)
 
-@app.route('/actualizar', methods=['GET','POST'])
+
+@app.route('/actualizar', methods=['GET', 'POST'])
 def actualizar():
     if request.method == "POST":
         qry = Alumno.query.get(request.form['id'])
         qry.nombre = request.form['nombreE']
-        qry.apellido =request.form['apellidoE']
+        qry.apellido = request.form['apellidoE']
         db.session.commit()
         return redirect(url_for('acerca'))
+
+
 if __name__ == "__main__":
     app.run()
